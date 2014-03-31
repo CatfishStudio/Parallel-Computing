c5279baf79cdf62822e9955ec17069ec9bff906c
CatfishStudio <somov@i.ua>
2014-03-31 09:46:34 +0300

    Create project: 31-03-2014  9:46

---
+++ MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: Catfish
+ * Дата: 31.03.2014
+ * Время: 9:39
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ParallelComputing
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: Catfish
+ * Дата: 31.03.2014
+ * Время: 9:39
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace ParallelComputing
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
