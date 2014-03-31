20c6d102f2335923ff02a4b49cff21f78bebb9c2
CatfishStudio <somov@i.ua>
2014-03-31 13:44:00 +0300

    Update: создан шаблон программы: 31-03-2014  13:43

--- Program.cs
+++ Program.cs
@@ -16,10 +16,36 @@ namespace ParallelComputing
 		public static void Main(string[] args)
 		{
 			// вызывается строкой: C:\GIT\ParallelComputing\bin\Debug>"C:\Program Files\Microsoft Compute Cluster Pack\Bin\mpiexec.exe" -n 8 ParallelComputing.exe
+			
 			using (new MPI.Environment(ref args))
-			{
+			{               
+               Intracommunicator comm = Communicator.world;
+            	if (comm.Rank == 0)
+            	{
+                	// program for rank 0
+    				comm.Send("Rosie", 1, 0);
+
+    				// receive the final message
+    				string msg = comm.Receive<string>(Communicator.anySource, 0);
+
+    				Console.WriteLine("Rank " + comm.Rank + " received message \"" + msg + "\".");
+            	}
+            	else // not rank 0
+            	{
+                	// program for all other ranks
+   					string msg = comm.Receive<string>(comm.Rank - 1, 0);
+
+    				Console.WriteLine("Rank " + comm.Rank + " received message \"" + msg + "\".");
+
+    				comm.Send(msg + ", " + comm.Rank, (comm.Rank + 1) % comm.Size, 0);
+                	
+            	}
+            	
+            	
+            	/*
 				Console.WriteLine("Hello, World! from rank " + Communicator.world.Rank
                   + " (running on " + MPI.Environment.ProcessorName + ")");
+                */
 			}
 			
 		}
